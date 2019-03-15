bc30feaf7709b993f2153e1d4361636ae369edce
jxq96 <jxq96@mail.ustc.edu.cn>
2019-03-15 16:19:42 +0800

    define some variables you will use

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 //
 // Creaed by jxq96 on 2019/3/10.
 //
-#include <stdio.h>
-int main(){
-
+#include <iostream>
+// unistd.h and getopt.h for parser command line parameters
+#include <unistd.h>
+#include <getopt.h>
+#include <string>
+using std::string;
+int main(int argc,char* argv[]){
+    const char* optstring = "w:c:h:t:n:";
+    bool choice = true; // most word or most character, true:word,false:character
+    bool head = false;
+    bool tail = false;
+    // if constraint head character or tail character
+    char headChar,tailChar;
+    string inputFileName;
 }
